e8e88b9d50c8b7a19383d934ddb9c94a08ed956e
Manuel Rivera <menu.rivera@gmail.com>
2022-11-12 19:39:03 -0600

    [MOD] -> 03-counter-app-vite complete

--- 03-counter-app-vite/src/components/Counter.jsx
+++ 03-counter-app-vite/src/components/Counter.jsx
@@ -8,17 +8,17 @@ const handleEvent = (e) => {
 export const Counter = ({ value }) => {
     const [counter, setCounter] = useState(value)
 
-    const handleAdd = () => {
-        setCounter(counter + 1)
-    }
+    const handleAdd = () => setCounter(counter + 1)
+    const handleSubstract = () => setCounter(counter - 1)
+    const handleReset = () => setCounter(value)
 
     return (
         <>
             <h1>Counter</h1>
             <h2>{counter}</h2>
-            <button onClick={handleAdd}>
-                +1
-            </button>
+            <button onClick={handleAdd}>+1</button>
+            <button onClick={handleSubstract}>-1</button>
+            <button onClick={handleReset}>Reset</button>
         </>
     )
 }
