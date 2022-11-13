9402ac5136f67a555a50eb2f8532e9a8fff16d90
Manuel Rivera <menu.rivera@gmail.com>
2022-11-12 20:50:45 -0600

    [MOD] -> Jest added to 03-counter-app-vite

---
+++ 03-counter-app-vite/test/base-pruebas/02-template-string.test.js
@@ -0,0 +1,10 @@
+import { getSaludo } from "../../src/base-pruebas/02-template-string";
+
+describe('Pruebas en 02-template-string', () => {
+    test('Prueba sobre getSaludo', () => {
+        const name = 'Manuel';
+        const msg = getSaludo(name);
+
+        expect(msg).toBe(`Hola ${name}  `)
+    })
+})
---
+++ 03-counter-app-vite/test/demo.test.js
@@ -0,0 +1,7 @@
+test('Test description', () => {
+    const msg1 = 'Hello world   '
+
+    const msg2 = msg1.trim()
+
+    expect(msg2).toBe(msg1)
+})
