8b0b7d93f1206a0fec32bf546d7bccfb5cc217ce
Manuel Rivera <menu.rivera@gmail.com>
2022-11-13 23:46:45 -0600

    [ADD] -> 04-gif-expert-app started

---
+++ 04-gif-expert-app/src/GifExpertApp.jsx
@@ -0,0 +1,10 @@
+import React from 'react'
+
+export const GifExpertApp = () => {
+    return (
+        <>
+            <h1>Gif Expert App</h1>
+            {/* <p>{import.meta.env.VITE_GIPHY_API_KEY}</p> */}
+        </>
+    )
+}
